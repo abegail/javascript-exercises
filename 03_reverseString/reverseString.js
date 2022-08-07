3bee7e17cafaf390584d336ed70c1ea2a8379b42
Abegail Pates <abegail.pates@gmail.com>
2022-08-07 13:15:29 +0800

    Finish exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const originalString = string.split('');
+    const reversedString = [];
 
+    for (i = originalString.length; i > 0; i--) {
+        reversedString.push(originalString[i - 1]);
+    }
+    return reversedString.join('');
 };
 
 // Do not edit below this line
