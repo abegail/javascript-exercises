81b5274bbad37ad57e27a6e4fef96b1e6f4f29f9
Abegail Pates <abegail.pates@gmail.com>
2022-08-07 16:16:15 +0800

    Finish exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
 const removeFromArray = function() {
+    const args = Array.from(arguments);
+    const array = args[0];
+    let itemToRemove;
 
+    for (j = 1; j < args.length; j++) {
+        itemToRemove = args[j];
+        for (i = 0; i < array.length; i++) {
+            if (array[i] === itemToRemove) {
+                array.splice(i, 1);
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
