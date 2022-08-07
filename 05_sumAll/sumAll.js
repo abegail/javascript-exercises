4c822f99f12e76410945615d343ff23e77dd6d96
Abegail Pates <abegail.pates@gmail.com>
2022-08-07 17:09:43 +0800

    Finish Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(firstInteger, secondInteger) {
 
+    if (firstInteger < 0 || secondInteger < 0 || typeof(firstInteger) !== 'number' || typeof(secondInteger) !== 'number') {
+        return 'ERROR';
+    }
+
+    let sum;
+    let lastInteger;
+    let nextInteger;
+
+    if (firstInteger < secondInteger) {
+        sum = firstInteger;
+        lastInteger = secondInteger
+        nextInteger = firstInteger + 1;
+    } else {
+        sum = secondInteger;
+        lastInteger = firstInteger
+        nextInteger = secondInteger + 1;
+    }
+
+    while (nextInteger <= lastInteger) {
+        sum += nextInteger;
+        nextInteger++;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
