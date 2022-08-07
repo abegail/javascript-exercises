03a7c060657b396fc9693b4be37d17f2eb5f20b7
Abegail Pates <abegail.pates@gmail.com>
2022-08-07 13:05:38 +0800

    Finish exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, numberOfTimes) {
+    let finalString = '';
 
+    if (numberOfTimes < 0) {
+        return 'ERROR';
+    }
+
+    for (i = numberOfTimes; i > 0; i--) {
+        finalString += string;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
