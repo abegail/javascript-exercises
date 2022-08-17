4caedaec66a06062d0acfcc533ba74379543462c
Abegail Pates <abegail.pates@gmail.com>
2022-08-17 18:51:22 +0800

    Finish Exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const originalString = Array.from(string.toLowerCase().replace(/[,!?.\s]/g, ""));
+    const reversedString = []
+    
+    for (letter in originalString) {
+        reversedString.unshift(originalString[letter]);
+    }
 
+    return originalString.join('') === reversedString.join('');
 };
 
 // Do not edit below this line
