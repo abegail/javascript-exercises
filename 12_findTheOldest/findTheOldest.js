a23f7406be92ef5a99abe810c7896ba26e5db3c8
Abegail Pates <abegail.pates@gmail.com>
2022-08-18 16:40:44 +0800

    Finish Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const ages = people.map(person => {
+        if (!('yearOfDeath' in person)) {
+            let currentYear = new Date().getFullYear();
+            return currentYear - person.yearOfBirth;
+        }
+        return person.yearOfDeath - person.yearOfBirth});
 
-};
+    const oldest = ages.reduce((oldest, person) => {
+            return person > oldest ? person : oldest;
+        }, 0)
+
+    return people[ages.indexOf(oldest)];
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
