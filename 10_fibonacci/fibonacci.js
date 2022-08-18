1cefed117937dfbd147f8b3dcb95c78dc0533958
Abegail Pates <abegail.pates@gmail.com>
2022-08-18 14:55:11 +0800

    Solve Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    fibo = [0, 1];
+    if (n == 0) {
+        return 0;
+    } else if (n == 1) {
+        return 1;
+    } else if (n < 0) {
+        return "OOPS"
+    } else {
+        for (i = 2; i <= n; i++) {
+            fibo.push(fibo[i - 1] + fibo[i - 2]);
+        }
+    }
+    return fibo[n];
 };
 
 // Do not edit below this line
